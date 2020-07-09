1412f1eb7b28d1c8839f8be942a2fd06be8605f4
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-09 18:40:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,17 +11,3 @@ const codes = [
   "a"
 ];
 
-function init() {
-  let index = 0;
-  
-  document.body.addEventListener("keydown", (e) => {
-     const key = e.key
-  
-  let idx = (codes[idx] === key) ? ++idx : 0
-  
-   if (idx === codes.length) {
-     window.alert("Hurray!");
-   let idx = 0
-   }
-  });
-}
