ff2be48a1b59cd493db569715b566b3b625e0976
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-09 18:41:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,3 +11,21 @@ const codes = [
   "a"
 ];
 
+function init() {
+
+        var index = 0;
+        const getElement = document.querySelector('body');
+
+        getElement.addEventListener('keydown', function(e) {
+            if (code[index] === e.which || code[index] === e.detail) {
+                index++;
+
+                if (index === code.length) {
+                    alert("CHEAT EXECUTED!!");
+                    index = 0;
+                }
+            } else {
+                index = 0;
+            }
+        })
+    }
