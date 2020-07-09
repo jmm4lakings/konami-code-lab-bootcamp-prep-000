656eed0c3602beccfb337ff5aad8dff534076f0b
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-09 18:28:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -26,3 +26,17 @@ function init() {
   });
 }
 
+unction init() {
+  let index = 0;
+  
+  document.body.addEventListener("keydown", (e) => {
+     const key = e.key
+  
+  let idx = (codes[idx] === key) ? ++idx : 0
+  
+   if (idx === codes.length) {
+     window.alert("Hurray!");
+   let idx = 0
+   }
+  });
+}
