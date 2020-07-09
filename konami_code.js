189a33b8ed3dd040bf70723b4505949bc0bc2496
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-09 18:25:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() {
   let index = 0;
   
-  document.body.addEventListener("keydown", (e) => {
+  document.body.addEventListener('keydown', (e) => {
      const key = e.key;
   
   let idx = (codes[idx] === key) ? ++idx : 0;
